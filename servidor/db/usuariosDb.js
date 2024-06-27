15baadeafb72c00471192b8b40f28e7133a8f607
Dirceusljr <dirceusljr@gmail.com>
2024-06-27 15:41:20 -0300

    criptografando a senha

--- servidor/db/usuariosDb.js
+++ servidor/db/usuariosDb.js
@@ -1,3 +1,4 @@
+import criaSalEHashSenha from "../utils/criaSalEHashSenha.js";
 import { usuariosColecao } from "./dbConnect.js";
 
 function encontrarUsuario(nome) {
@@ -5,7 +6,9 @@ function encontrarUsuario(nome) {
 }
 
 function cadastrarUsuario({ nome, senha }) {
-    return usuariosColecao.insertOne({ nome, senha });
+    const { salSenha, hashSenha } = criaSalEHashSenha(senha);
+
+    return usuariosColecao.insertOne({ nome, salSenha, hashSenha });
 }
 
 export { cadastrarUsuario, encontrarUsuario }
