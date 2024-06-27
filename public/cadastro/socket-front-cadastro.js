5dcb9ff9826a543f2cebd44c065be93e5183bbfa
Dirceusljr <dirceusljr@gmail.com>
2024-06-27 15:25:00 -0300

    criando coleção de usuario + conexao com a nova coleção + cadastro do usuario com tratativa de erros

--- public/cadastro/socket-front-cadastro.js
+++ public/cadastro/socket-front-cadastro.js
@@ -4,4 +4,8 @@ function emitirCadastroUsuario (dados) {
     socket.emit("cadastro_usuario", dados)
 }
 
+socket.on("cadastro_sucesso", () => alert("Usuário cadastrado com sucesso!"));
+socket.on("cadastro_erro", () => alert("Erro no cadastro."));
+socket.on("usuario_existente", () => alert("O usuário já existe!"));
+
 export { emitirCadastroUsuario }
--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -2,17 +2,18 @@ import { MongoClient } from "mongodb";
 
 const cliente = new MongoClient(process.env.DB_URL);
 
-let documentosColecao;
+let documentosColecao, usuariosColecao;
 
 try {
   await cliente.connect();
 
   const db = cliente.db("alura-websockets2");
   documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
 
   console.log("Conectado ao banco de dados com sucesso!");
 } catch (erro) {
   console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
