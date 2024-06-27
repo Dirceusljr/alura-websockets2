48a8d4ffbca7d2204e3cc2c7f532e9c06bf7776f
Dirceusljr <dirceusljr@gmail.com>
2024-06-27 18:42:30 -0300

    manipulando tokenJwt com cookies no front

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,10 +1,13 @@
+import { definirCookie } from "../utils/cookie.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados) {
     socket.emit("autenticar_usuario", dados)
 }
 
-socket.on("autenticacao_sucesso", () => {
+socket.on("autenticacao_sucesso", (tokenJwt) => {
+    definirCookie("tokenJwt", tokenJwt)
     alert("Autenticação realizada com sucesso!");
     window.location.href = '/';
 })
